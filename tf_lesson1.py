e1de6076b25c6dfc9e161e53b3dc3d5956ba6b6c
Lee Yeong Khang <sindrosa.24@gmail.com>
2015-11-16 23:06:27 +0800

    add new lesson

--- tf_lesson1.py
+++ tf_lesson1.py
@@ -6,21 +6,19 @@ from algo.helper import loaddata_tf
 from sklearn import preprocessing
 import numpy as np
 
-# import tensorf.mnist as input_data
-# mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
 
 lb = preprocessing.LabelBinarizer()
-
 mnist = loaddata_tf("./data/mnist.pkl.gz")
 train_x, train_y = mnist[0]
+valid_x, valid_y = mnist[1]
 test_x, test_y = mnist[2]
 
-batch_no = 1000
+batch_no = 2000
 batch_size = train_x.shape[0] / batch_no
 
 lb.fit(range(10))
 train_y = lb.transform(train_y)
+valid_y = lb.transform(valid_y)
 test_y = lb.transform(test_y)
 
 # Create the modela
@@ -39,18 +37,28 @@ init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(1000):
-	# original code use this, but our code load mnist differently
-	# batch_xs, batch_ys = mnist.train.next_batch(100)
-	ind = i * batch_size
-	batch_xs = train_x[ind: ind+batch_no]
-	batch_ys = train_y[ind: ind+batch_no]
-	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-# Test trained model
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-# print(sess.run(accuracy, feed_dict={x: train_x, y_: train_y}))
 
-print(sess.run(accuracy, feed_dict={x: test_x, y_: test_y}))
-# print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print("Training starting")
+done_train = False
+valid_score = 0
+while done_train is False:
+	for i in range(batch_no):
+		ind = i * batch_size
+		batch_xs = train_x[ind: ind+batch_no]
+		batch_ys = train_y[ind: ind+batch_no]
+		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	print("train accuracy", sess.run(accuracy, feed_dict={x: train_x, y_: train_y}))
+	v_s = sess.run(accuracy, feed_dict={x: valid_x, y_: valid_y})
+	print("   valid accuracy", v_s)
+	
+	improvement =  v_s - valid_score
+	valid_score = v_s
+
+	if(improvement < 0.001):
+		done_train = True
+# Test trained model
+
+print("train accuracy", sess.run(accuracy, feed_dict={x: train_x, y_: train_y}))
+print("test accuracy", sess.run(accuracy, feed_dict={x: test_x, y_: test_y}))
